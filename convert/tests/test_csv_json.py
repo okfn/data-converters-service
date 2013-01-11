639b0eb1e65242daac9ea9b92b29860b62441418
Nigel Babu <nigelbabu@gmail.com>
2013-01-11 11:23:01 +0530

    Remove the web folder

--- convert/tests/test_csv_json.py
+++ convert/tests/test_csv_json.py
@@ -8,7 +8,7 @@ class TestCase(TestCase):
     def setUp(self):
         self.app = app.test_client()
         here = os.path.dirname(os.path.abspath(__file__))
-        self.config_path = os.path.dirname(os.path.dirname(os.path.dirname(here)))
+        self.config_path = os.path.dirname(os.path.dirname(here))
         app.config.from_pyfile(os.path.join(self.config_path, 'settings.py'))
         app.config.from_pyfile(os.path.join(self.config_path, 'test_settings.py'),
                                silent=True)
--- convert/tests/test_errors.py
+++ convert/tests/test_errors.py
@@ -8,9 +8,9 @@ class TestCase(TestCase):
     def setUp(self):
         self.app = app.test_client()
         here = os.path.dirname(os.path.abspath(__file__))
-        config_path = os.path.dirname(os.path.dirname(os.path.dirname(here)))
-        app.config.from_pyfile(os.path.join(config_path, 'settings.py'))
-        app.config.from_pyfile(os.path.join(config_path, 'test_settings.py'),
+        self.config_path = os.path.dirname(os.path.dirname(here))
+        app.config.from_pyfile(os.path.join(self.config_path, 'settings.py'))
+        app.config.from_pyfile(os.path.join(self.config_path, 'test_settings.py'),
                                silent=True)
 
     def test_1_convert_params_with_url(self):
