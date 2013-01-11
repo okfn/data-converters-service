a094db29494ded397e9d12e671643e36d6fa48e6
Nigel Babu <nigelbabu@gmail.com>
2013-01-11 11:07:53 +0530

    Re-enable POST endpoint

--- convert/views.py
+++ convert/views.py
@@ -23,8 +23,9 @@ def index():
 @crossdomain(origin='*', headers=cors_headers)
 @jsonpify
 def convert_get(targetformat='json'):
-    metadata = request.args.to_dict()
+
     # Error checking
+    metadata = request.args.to_dict()
     url = request.args.get('url', None)
     originformat = request.args.get('type', None)
     if url is None:
@@ -39,6 +40,8 @@ def convert_get(targetformat='json'):
     r = requests.get(url)
     if requests.codes.ok != r.status_code:
         return error("Could't access the file at {0}".format(url))
+
+    # Convert
     handle = StringIO(r.content)
     try:
         results, metadata = module.parse(handle, **metadata)
@@ -47,26 +50,28 @@ def convert_get(targetformat='json'):
         return error(str(e))
     return Response(results_json, mimetype='application/json')
 
-"""
+
 @app.route('/api/convert/<targetformat>', methods=['POST'])
 @crossdomain(origin='*', headers=cors_headers)
 @jsonpify
-def convert_post(targetformat=None):
+def convert_post(targetformat='json'):
+
+    # Error checking
     uploaded_file = request.files['file']
     metadata = request.form.to_dict()
-    results = {}
-    metadata['target'] = targetformat
-    if not uploaded_file or targetformat is None:
-        results['error'] = 'No file uploaded or format specified'
-        results_json = json.dumps(results)
-        return Response(results_json, mimetype='application/json')
-    metadata['mime_type'] = uploaded_file.content_type
+    originformat = request.form.get('type', None)
+    if not uploaded_file:
+        return error('No file given')
+    if targetformat is None or originformat is None:
+        return error('No format or type specified')
+    module = converters.get(originformat, None)
+    if module is None:
+        return error('No converter found for {0}',format(originformat))
+
+    # Convert
     try:
-        data = dataconverter(uploaded_file.stream, metadata)
-        header, results = data.convert()
-        results_json = json.dumps({'headers': header, 'data': results})
+        results, metadata = module.parse(uploaded_file.stream, **metadata)
+        results_json = json.dumps({'metadata': metadata, 'records': list(results)}, cls=IteratorEncoder)
     except Exception as e:
-        results['error'] = str(e)
-        results_json = json.dumps(results)
+        return error(str(e))
     return Response(results_json, mimetype='application/json')
-"""
